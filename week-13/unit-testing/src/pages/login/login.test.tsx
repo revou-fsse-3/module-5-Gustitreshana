0755b4e21cfac8ad38d31f2d9c9ae9c7fe992003
Gustitreshana <gustith2105@gmail.com>
2024-01-31 23:15:42 +0700

    fix error in Login page

--- week-13/unit-testing/src/pages/login/login.test.tsx
+++ week-13/unit-testing/src/pages/login/login.test.tsx
@@ -1,20 +1,47 @@
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import "@testing-library/jest-dom"
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import LoginPage from '.';
+import fetchMock, { FetchMock } from 'jest-fetch-mock';
+
+fetchMock.enableMocks();
+const mockOnSubmit = jest.fn();
 
 describe('LoginPage', () => {
-  it('submits the form', async () => {
-    const mockOnSubmit = jest.fn();
-    const { getByLabelText, getByText } = render(<LoginPage onSubmit={mockOnSubmit} />);
 
-    fireEvent.change(getByLabelText('Email'), { target: { value: 'test@email.com' } });
-    fireEvent.change(getByLabelText('Password'), { target: { value: 'Test@1234' } });
-    fireEvent.click(getByText('Login now!'));
+  it('renders correctly', () => {
 
-    await waitFor(() => expect(mockOnSubmit).toHaveBeenCalled());
+    render(<LoginPage onSubmit={mockOnSubmit}/>)
 
-    expect(mockOnSubmit).toHaveBeenCalledWith({
-      email: 'gusti@gmail.com',
-      password: 'Test1234',
+    const emailInput = screen.getByPlaceholderText("email")
+    expect(emailInput).toBeDefined();
+
+    const passInput = screen.getByPlaceholderText("password")
+    expect(passInput).toBeDefined();
+
+    const buttonLogin = screen.getByText("Login now!")
+  });
+  
+  it('handles form submission', async () => {
+    
+    (fetch as FetchMock).mockResponseOnce(JSON.stringify({ success: true }));
+    render(<LoginPage onSubmit={mockOnSubmit}/>)
+  
+    const emailInput = screen.getByPlaceholderText("email");
+    const passInput = screen.getByPlaceholderText("password")
+    const buttonLogin = screen.getByText("Login now!")
+
+    fireEvent.change(emailInput, {target: {value: "gusti@gmail.com"}});
+    fireEvent.change(passInput, {target: {value: "Test1234"}});
+    fireEvent.click(buttonLogin);
+
+
+    await waitFor(() => {
+      expect(mockOnSubmit).toHaveBeenCalled();
+    });
+      
+      expect(mockOnSubmit).toHaveBeenCalledWith({
+        email: 'gusti@gmail.com',
+        password: 'Test1234',
     });
   });
 });
